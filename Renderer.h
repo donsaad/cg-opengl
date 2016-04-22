1fa5d71517af074d09449ef2b6d9a1cdef37d005
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2016-04-22 16:14:56 +0200

    Draw red triangle on left and blue square on right (assignment 2)

--- Renderer.h
+++ Renderer.h
@@ -1,26 +1,18 @@
 #include <gl/glew.h>
 #include "shader.hpp"
-#include <glm/gtx/transform.hpp>
-using namespace glm;
 #pragma once
 
-class Renderer
-{
-    GLuint vertexArrayID;
-    GLuint vertexBufferID;
-    GLuint programID;
-	
-	GLuint MatrixID;
-	mat4 ProjectionMatrix;
-	mat4 ViewMatrix;
-	mat4 ModelMatrix;
-	mat4 MVP;
+class Renderer {
+	GLuint vertexArrayID;
+	GLuint vertexBufferID;
+	GLuint programID;
+
 
 public:
-    Renderer();
-    ~Renderer();
-    
-    void Initialize();
-    void Draw();
-    void Cleanup();
+	Renderer();
+	~Renderer();
+
+	void Initialize();
+	void Draw();
+	void Cleanup();
 };
